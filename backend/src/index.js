f80c5b5536b62083c3dc30ff15a61f2294864e4f
Asif Shamim <asifshamim12816@gmail.com>
2026-06-13 22:00:44 +0530

    Add message handling routes and media upload functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,8 +11,10 @@ import { clerkMiddleware } from "@clerk/express";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
+
 import clerkWebhook from "./webhooks/clerk.webhook.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -33,6 +35,7 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 // if the public directory exists, serve the static files
 // this is for the production build
